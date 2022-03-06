4a2a3d7a83ac295aabe1395c7fc11bbd4367416f
ahmedaof <49912597+ahmedaof@users.noreply.github.com>
2022-03-06 20:42:40 +0200

    Update create.vue

--- vueTask/src/components/brand/create.vue
+++ vueTask/src/components/brand/create.vue
@@ -1,51 +1,37 @@
- 
-
 <template>
-  
+
   <div>
 
  <div class="row">
-  <router-link to="/brand" class="btn btn-primary">All Brands </router-link>
+  <router-link to="/brand" class="btn btn-primary add">All Brand </router-link>
    
  </div>
 
+   <h1 class="h4 text-gray-900 mb-4">Add Brand </h1>
+   
+<v-container>
+    <v-form ref="form"  lazy-validation>
+      <v-row class="text-center">
+        <v-text-field
+          v-model="form.name"
+          label="name"
+          required
+        ></v-text-field>
 
-
-    <div class="row justify-content-center">
-      <div class="col-xl-12 col-lg-12 col-md-12">
-        <div class="card shadow-sm my-5">
-          <div class="card-body p-0">
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="login-form">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Add Brand</h1>
-                  </div>
-
-      <form class="user" @submit.prevent="brandInsert">
-
-        <div class="form-group">
-
-          <div class="form-row">
-            <div class="col-md-12">
-         <input type="text" class="form-control" id="exampleInputFirstName" placeholder="Enter Your Brand Name" v-model="form.name">
-  <small class="text-danger" v-if="errors.name"> {{ errors.name[0] }} </small>
-           
-
-            </div> 
-            
-          </div>
-        </div>
-       
-         
- 
-
-
-        <div class="form-group">
-          <button type="submit" class="btn btn-primary btn-block">Submit</button>
-        </div>
+      </v-row>
+          <small class="text-danger" v-if="errors.name"> {{ errors.name[0] }} </small>
+      </v-form>
+      </v-container>
+        <v-row class="text-center">
+        <v-btn
+          color="success"
+          class="mr-4"
+          @click="brandInsert"
+        >
+           add Brand
+        </v-btn>
+      </v-row>
         
-      </form>
                   <hr>
                   <div class="text-center">
   
@@ -54,16 +40,10 @@
                   <div class="text-center">
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 
 
 </template>
+
 
 
 
@@ -72,8 +52,18 @@ import { API } from "../../config/config"
 import axios from 'axios'
   
   export default {
- 
-
+created(){
+    if(User.IsAdmin() ==0){
+         this.$swal({
+              title: 'this action for admins only?',
+              icon: 'warning',
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+            }).then((result) => {
+              })
+               this.$router.push({ name: 'home'})
+    }
+},
     data(){
     return {
       form:{
@@ -89,9 +79,8 @@ import axios from 'axios'
   brandInsert(){
        axios.post(`${API}/api/brand`,this.form)
        .then((res) => {
-         
+         console.log(res)
         this.$router.push({ name: 'brand'})
-        Notification.success()
        })
        .catch(error =>this.errors = error.response.data.errors)
      },
